9cc2a913ae5d60ccca493371ae78cd6c4625a52e
BOTOOM <eddiazr@correo.udistrital.edu.co>
2019-09-29 20:21:06 -0500

    se crea base del service json

--- src/app/pages/ante-proyecto/ante-proyecto.component.ts
+++ src/app/pages/ante-proyecto/ante-proyecto.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LocaljsonService } from '../../services/localjson.service';
+
 
 @Component({
   selector: 'app-ante-proyecto',
@@ -7,7 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnteProyectoComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    local: LocaljsonService
+  ) { }
 
   ngOnInit() {
   }
