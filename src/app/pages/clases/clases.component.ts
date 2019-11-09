fd946d988e96bc040dd8977fcd476f1266e7efe1
BOTOOM <eddiazr@correo.udistrital.edu.co>
2019-11-09 16:38:01 -0500

    numero de clase, fecha,e inicio de la tabla

--- src/app/pages/clases/clases.component.ts
+++ src/app/pages/clases/clases.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LocaljsonService } from '../../services/localjson.service';
 
 @Component({
   selector: 'app-clases',
@@ -7,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClasesComponent implements OnInit {
   panelOpenState = false;
+  DatosClases: any;
+  Tam_row: number;
 
-  constructor() { }
+  constructor(
+    private local: LocaljsonService
+  ) { }
 
   ngOnInit() {
+    this.local.get('clases').subscribe( dato => {
+      // console.log(dato);
+      this.DatosClases = dato;
+      this.Tam_row = this.DatosClases.length;
+      console.info(this.Tam_row)
+      console.info(this.DatosClases)
+    }, (error_service) => {
+      console.log(error_service);
+    });
   }
 
 }
