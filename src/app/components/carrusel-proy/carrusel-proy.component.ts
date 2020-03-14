6088e168d937a0ef4463b7ed620324b1ea63bc8a
BOTOOM <eddiazr@correo.udistrital.edu.co>
2020-03-14 13:23:42 -0500

    add componet carrusel proy

---
+++ src/app/components/carrusel-proy/carrusel-proy.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-carrusel-proy',
+  templateUrl: './carrusel-proy.component.html',
+  styleUrls: ['./carrusel-proy.component.scss']
+})
+export class CarruselProyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
